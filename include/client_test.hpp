c6653a2d8b4ef8bd7c7ce6529e617b3139d5f089
rcktscnc <itsucksthemost@gmail.com>
2017-03-12 05:11:53 -0300

    remote server client support

--- include/client_test.hpp
+++ include/client_test.hpp
@@ -16,3 +16,32 @@ void client()
         std::cout.write(buf, len);
     }
 }
+
+void codenvy_client()
+{
+    asio::io_service io_service;
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query("node11.codenvy.io", "41996");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+    tcp::socket socket(io_service);
+    
+    /*tcp::endpoint endpoint;
+
+    while (endpoint_iterator != tcp::resolver::iterator())
+    {
+        endpoint = *endpoint_iterator++;
+        std::cout << endpoint << std::endl;
+    }
+
+   socket.connect(endpoint);*/
+
+    asio::connect(socket, endpoint_iterator);
+
+    asio::error_code err;
+    char buf[512] = { 0 };
+    for (;;)
+    {
+        std::size_t len = socket.read_some(asio::buffer(buf), err);
+        std::cout.write(buf, len);
+    }
+}
